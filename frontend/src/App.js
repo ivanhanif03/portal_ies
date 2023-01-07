b6e6f4d187c3877b16650e22ae9c367eae1d516c
M Ivan Hanif V <ivanhanif03@gmail.com>
2023-01-07 11:40:21 +0700

    update all frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import AppList from "./components/AppList";
-import AddApp from "./components/AddApps";
-import EditApp from "./components/EditApps";
 
 function App() {
   return (
-    <Router>
-    <div className="container">
-      <Switch>
-        <Route exact path="/">
-          <AppList />
-        </Route>
-        <Route path="/add">
-          <AddApp />
-        </Route>
-        <Route path="/edit/:id">
-          <EditApp />
-        </Route>
-      </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </Router>
   );
 }
 
